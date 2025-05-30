7ad4a5b71b1a3db2c3f25647b07edd978d079c0a
KNithyakala <cknithyakala@gmail.com>
2025-05-30 18:38:58 +0530

    Delete ratedsortedlist.py

--- ratedsortedlist.py
+++
@@ -1,8 +0,0 @@
-#Create a rated and sorted list
-#find minimum element in rated and sorted list
-
-
-List1=[("Task1",4),("Task2",10),("Task1",6),("Task3",8)]
-List1.sort()
-print("Rated and Sorted List:", List1)
-print("Minimum element in rated and sorted List:", List1[0])
